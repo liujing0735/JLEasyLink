3c221c5fba174d8034f35ac7addf6ea7c6ef47d4
JasonLiu <jasonliu@JasonLiudeMacBook-Air.local>
2018-08-30 10:20:41 +0800

    first commit

---
+++ JLEasyLink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JLEasyLink
+//
+//  Created by JasonLiu on 2018/8/28.
+//  Copyright © 2018年 trudian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JLEasyLink/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JLEasyLink
+//
+//  Created by JasonLiu on 2018/8/28.
+//  Copyright © 2018年 trudian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
